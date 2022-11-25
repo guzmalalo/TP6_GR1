feffcd517ad0f6c6fac41d37b1aa7acff441edff
Eduardo Guzman <eduardo.guzman@innovamics.com>
2022-11-25 12:41:01 +0100

    Corrigé exo 3, version sans procedure

---
+++ exo3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "exo3.h"
+
+/**
+ * EXO 3 TP 6
+ */
+int main(){
+    // Déclaration
+    int a, b, temp;
+
+    // Initialisation de a et b
+    printf("Rentrez la valeur de a : ");
+    scanf("%d", &a);
+
+    printf("Rentrez la valeur de b : ");
+    scanf("%d", &b);
+
+    //avant
+    printf("Avant\n");
+    printf("a = %d b = %d \n", a, b);
+
+    //swap
+    temp = a;
+    a=b;
+    b=temp;
+
+    printf("Apres\n");
+    printf("a = %d b = %d \n", a, b);
+
+    return 0;
+}
---
+++ exo3.h
@@ -0,0 +1,5 @@
+
+#ifndef TP6_EXO3_H
+#define TP6_EXO3_H
+
+#endif //TP6_EXO3_H
