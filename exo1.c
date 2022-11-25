34c1113a2ff8435abed5b7c0bcf382088013dadb
Eduardo Guzman <eduardo.guzman@innovamics.com>
2022-11-25 11:53:37 +0100

    Correction exo 1 tp 6

--- exo1.c
+++ exo1.c
@@ -1,5 +1,28 @@
+#include <stdio.h>
 #include "exo1.h"
 
-int main(){
+/**
+ * Exo 1 TP 5
+ */
+int main() {
+    int a;
+    float b;
+
+    //avant
+    printf("Avant\n");
+    printf("a = %d b = %f \n", a, b);
+
+    initialize(&a, &b);
+
+    printf("Apres\n");
+    printf("a = %d b = %f \n", a, b);
+
     return 0;
+}
+
+void initialize(int *pa, float *pb) {
+    *pa = 0;
+    *pb = 0;
+    printf("adresse de pa %x \n", pa);
+    printf("adresse de pa %p \n", pb);
 }
--- exo1.h
+++ exo1.h
@@ -5,4 +5,11 @@
 #ifndef TP6_GR1_EXO1_H
 #define TP6_GR1_EXO1_H
 
+/**
+ * Initialise un entier et un réel
+ * @param pa  pointeur vers un entier a
+ * @param pb  pointeur vers un entier b
+ */
+void initialize(int* pa, float* pb);
+
 #endif //TP6_GR1_EXO1_H
